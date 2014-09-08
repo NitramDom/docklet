b3d12933aaed60cd91f87589f038724e5a26f8fc
slopjong <git@slopjong.de>
2014-09-08 19:30:13 +0200

    New commands: attach, build, commit, cp, diff, events, export, images (implemented), info, ps (implemented, run

---
+++ src/Docklet/Command/Info.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Docklet (http://slopjong.github.io/docklet)
+ *
+ * @link      http://github.com/slopjong/docklet for the canonical source repository
+ * @copyright Copyright (c) 2014 Romain Schmitz (http://slopjong.de)
+ * @license   http://slopjong.github.io/docklet/license/new-bsd New BSD License
+ */
+
+namespace Docklet\Command;
+
+
+class Info extends AbstractCommand
+{
+} 
