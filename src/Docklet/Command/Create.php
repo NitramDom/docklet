a099b2ebd327f7b5d1ffa2dcb114bcabef1c1862
slopjong <git@slopjong.de>
2014-09-16 19:30:13 +0200

    Implemented the 'create' command

--- src/Docklet/Command/Create.php
+++ src/Docklet/Command/Create.php
@@ -10,6 +10,39 @@
 namespace Docklet\Command;
 
 
+use Docklet\Container\Container;
+use Zend\Http\Request;
+
+/**
+ * Class Create
+ *
+ * @package Docklet\Command
+ * @link https://docs.docker.com/reference/api/docker_remote_api_v1.14/#create-a-container
+ */
 class Create extends AbstractCommand
 {
-} 
+    /**
+     * @var Container
+     */
+    protected $container = null;
+
+    /**
+     * @param Container $container
+     */
+    public function __construct(Container $container)
+    {
+        $this->container = $container;
+        $this->setMethod(Request::METHOD_POST);
+        $this->setCommand('containers/create');
+        $this->setContent($container->toJson());
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function execute()
+    {
+        $this->getUri()->setPath($this->command);
+        return $this;
+    }
+}
