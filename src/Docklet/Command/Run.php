e540191e73c20b4d98c99ee517dd5be7db2c62d3
slopjong <git@slopjong.de>
2014-09-17 19:30:13 +0200

    Implemented the 'run' command partly, the start command is yet missing

--- src/Docklet/Command/Run.php
+++ src/Docklet/Command/Run.php
@@ -9,7 +9,45 @@
 
 namespace Docklet\Command;
 
+use Zend\Http\Response;
 
+/**
+ * Class Run
+ *
+ * @package Docklet\Command
+ * @link https://docs.docker.com/reference/api/docker_remote_api_v1.14/#31-inside-docker-run
+ */
 class Run extends AbstractCommand
 {
+    protected $container = null;
+
+    public function __construct(Container $container)
+    {
+        $this->container = $container;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function execute()
+    {
+        $create = new Create($this->container);
+        return $create->execute();
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function postExecute(Response $response)
+    {
+        if ($response->getStatusCode() === Response::STATUS_CODE_404) {
+            // @todo: the image wasn't found, try to pull it
+            //        and throw an exception if that wasn't found too
+        }
+
+        // @todo: start the container
+        // @todo: return the container ID if in detached mode
+
+        return $response->getBody();
+    }
 } 
