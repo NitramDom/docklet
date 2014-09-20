e622c113b4e917e91c70e3099b2a538eff3e7fba
slopjong <git@slopjong.de>
2014-09-20 20:34:53 +0200

    New example added

---
+++ examples/run_apache.php
@@ -0,0 +1,28 @@
+<?php
+
+include('../vendor/autoload.php');
+
+use Docklet\Docker;
+use Docklet\Command\Run;
+
+// 1. Create a new Docker client
+//$docker = new Docker('unix:///var/run/docker.sock');
+$docker = new Docker('tcp://127.0.0.1:9999');
+
+// 2. Create the config for the run command
+$command = array(
+    '/usr/sbin/apache2ctl',
+    '-D',
+    'FOREGROUND',
+);
+
+$options = Run::options('slopjong/apache', $command)
+    ->portBinding('80', '8999')
+    ->daemon(true);
+
+// 3. Run a container and point your browser to http://localhost:8999
+$json = $docker->run($options);
+
+// 4. Output the container ID
+$stdObj = json_decode($json);
+echo $stdObj->Id;
