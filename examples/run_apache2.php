1bcc6f0e54c04da27d80e348242716b00c4f9124
slopjong <git@slopjong.de>
2014-09-20 20:46:53 +0200

    Added another apache example.

---
+++ examples/run_apache2.php
@@ -0,0 +1,33 @@
+<?php
+
+include('../vendor/autoload.php');
+
+use Docklet\Docker;
+use Docklet\Command\Run;
+use Docklet\Container\Container;
+
+// 1. Create a new Docker client
+//$docker = new Docker('unix:///var/run/docker.sock');
+$docker = new Docker('tcp://127.0.0.1:9999');
+
+// 2. Create the config for the run command
+$command = array(
+    '/usr/sbin/apache2ctl',
+    '-D',
+    'FOREGROUND',
+);
+
+$options = Run::options('slopjong/apache', $command)
+    ->portBinding('80', '8999')
+    ->daemon(true)
+    ->returnContainer(true);
+
+// 3. Run a container and point your browser to http://localhost:8999
+/** @var Container $container */
+$container = $docker->run($options);
+
+// 4. Output the container ID
+echo $container->getId();
+
+// 5. Stop the container after 10 seconds
+$docker->stop($stdObj->Id, 10);
