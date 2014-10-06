ee1e4b47958574d0b0d05abf12e7215ba872e729
slopjong <git@slopjong.de>
2014-10-06 19:47:54 +0200

    Docblock and todo added.

--- src/Docklet/Command/Ps/Ps.php
+++ src/Docklet/Command/Ps/Ps.php
@@ -9,6 +9,7 @@
 
 namespace Docklet\Command\Ps;
 use Docklet\Command\AbstractCommand;
+use Docklet\Container\Container;
 use Zend\Http\Headers;
 use Zend\Http\Request;
 use Zend\Http\Response;
@@ -47,8 +48,24 @@ class Ps extends AbstractCommand
         return $this;
     }
 
+    /**
+     * Returns a JSON as follows.
+     *
+     * <pre><code>[{
+     *  "Command":"/usr/sbin/apache2ctl -D FOREGROUND","Created":1411428466,
+     *  "Id":"2474fcbf399e61adfe2e3faad8fbe7fd50f1a4ade93b0cf0112d2716e4b92f01",
+     *  "Image":"slopjong/apache:latest","Names":["/angry_turing"],
+     *  "Ports":[],"Status":"Up Less than a second"
+     * }]</code></pre>
+     *
+     * @param Response $response
+     * @param bool $returnContainer
+     * @return \Docklet\Container\Container|string
+     */
     public function postExecute(Response $response, $returnContainer = false)
     {
+        // @todo: Decode the JSON and create proper container objects.
+        //        Then return the container list.
         return $response->getBody();
     }
 
