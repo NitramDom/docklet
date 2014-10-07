61f1cad0728c072d2c900c8257fa9faddeda1839
slopjong <git@slopjong.de>
2014-10-07 19:47:54 +0200

    Throwing an exception in Stop::postExecute() if something went wrong. Unit test for Stop implemented.

--- src/Docklet/Command/Stop/Stop.php
+++ src/Docklet/Command/Stop/Stop.php
@@ -11,18 +11,24 @@ namespace Docklet\Command\Stop;
 
 
 use Docklet\Command\AbstractCommand;
+use Docklet\Exception\NoSuchContainerException;
+use Docklet\Exception\ServerErrorException;
 use Zend\Http\Request;
 use Zend\Http\Response;
 
 
 /**
- * Class Stop
+ * Stop a running container by sending SIGTERM and then SIGKILL after a grace period.
  *
  * @package Docklet\Command
  * @link https://docs.docker.com/reference/api/docker_remote_api_v1.14/#stop-a-container
  */
 class Stop extends AbstractCommand
 {
+    /**
+     * @param $id container ID or name
+     * @param int $wait Number of seconds to wait for the container to stop before killing it. Default is 10 seconds.
+     */
     public function __construct($id, $wait = 10)
     {
         $this->setMethod(Request::METHOD_POST);
@@ -37,5 +43,18 @@ class Stop extends AbstractCommand
     {
         $this->getUri()->setPath($this->command);
         return $this;
+    }
+
+    public function postExecute(Response $response, $returnContainer = false)
+    {
+        // @todo: document in the interface what exceptions can be thrown
+        switch($response->getStatusCode()) {
+            case Response::STATUS_CODE_404:
+                throw new NoSuchContainerException();
+                break;
+            case Response::STATUS_CODE_500:
+                throw new ServerErrorException();
+                break;
+        }
     }
 } 
---
+++ tests/DockletTest/Command/StopTest.php
@@ -0,0 +1,73 @@
+<?php
+/**
+ * Docklet (http://slopjong.github.io/docklet)
+ *
+ * @link      http://github.com/slopjong/docklet for the canonical source repository
+ * @copyright Copyright (c) 2014 Romain Schmitz (http://slopjong.de)
+ * @license   http://slopjong.github.io/docklet/license/new-bsd New BSD License
+ */
+
+namespace DockletTest\Command;
+
+
+// the auto-loader is not configured for unit test cases, including manually here
+include_once('DockletTest/DockerTestCase.php');
+
+use Docker\Docker;
+use Docklet\Command\Run\Run;
+use Docklet\Command\Stop\Stop;
+use DockletTest\DockerTestCase;
+
+class StopTest extends DockerTestCase
+{
+    protected static $image = 'slopjong/apache';
+    protected static $container_command = array(
+        '/usr/sbin/apache2ctl',
+        '-D',
+        'FOREGROUND',
+    );
+
+    public function testStopById()
+    {
+        $options = Run::options(static::$image, static::$container_command)
+            ->daemon(true);
+
+        $container = static::$docker->run($options);
+        $stdObj = json_decode($container);
+        $containerId = substr($stdObj->Id, 0, 12);
+
+        $containers = static::dockerPs($runningOnly = true);
+        $found = preg_match('/'. $containerId .'/m', $containers);
+        $this->assertTrue(filter_var($found, FILTER_VALIDATE_BOOLEAN));
+
+        static::$docker->stop($containerId);
+
+        $containers = static::dockerPs($runningOnly = true);
+        $found = preg_match('/'. $containerId .'/m', $containers);
+        $this->assertFalse(filter_var($found, FILTER_VALIDATE_BOOLEAN));
+
+        static::$removeContainersWithId[] = $containerId;
+    }
+
+    public function testStopByName()
+    {
+        $containerName = 'testTest';
+        $options = Run::options(static::$image, static::$container_command)
+            ->name($containerName)
+            ->daemon(true);
+
+        static::$docker->run($options);
+
+        $containers = static::dockerPs($runningOnly = true);
+        $found = preg_match('/'. $containerName .'/m', $containers);
+        $this->assertTrue(filter_var($found, FILTER_VALIDATE_BOOLEAN));
+
+        static::$docker->stop($containerName);
+
+        $containers = static::dockerPs($runningOnly = true);
+        $found = preg_match('/'. $containerName .'/m', $containers);
+        $this->assertFalse(filter_var($found, FILTER_VALIDATE_BOOLEAN));
+
+        static::$removeContainersWithId[] = $containerName;
+    }
+}
